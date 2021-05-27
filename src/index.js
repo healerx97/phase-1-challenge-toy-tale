793a00508263092a8ecac302d27c551b2fe80b3e
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-27 02:28:18 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -13,3 +13,62 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+let toyCollection = document.querySelector('#toy-collection')
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp=>resp.json())
+  .then(function(data) {
+    data.forEach(toy => {
+      let div = document.createElement('div')
+      div.className = "card"
+      div.innerHTML = `<h2>${toy.name}</h2>
+      <img src='${toy.image}' class = 'toy-avatar'>`
+      let p = document.createElement('p')
+      p.textContent = `${toy.likes} Likes`
+      div.append(p)
+      let btn = document.createElement('button')
+      btn.className = 'like-btn'
+      btn.id = `${toy.id}`
+      btn.textContent = 'Like'
+      btn.addEventListener('click', () => {
+        fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify({
+          "likes": toy.likes + 1
+        })
+        })
+        .then(p.textContent = `${toy.likes + 1} Likes`)
+        })
+      div.append(btn)
+      toyCollection.append(div)
+    })
+  })
+}
+
+document.addEventListener("DOMContentLoaded", getToys)
+
+let form = document.querySelector('.add-toy-form')
+function postToys(e) {
+  e.preventDefault()
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      'name' : `${form.children[1].value}`,
+      'image': `${form.children[3].value}`,
+      "likes": 0
+    })
+  })
+  .then(()=>{
+    form.children[1].value = ""
+    form.children[3].value = ""
+  })
+}
+form.addEventListener('submit', postToys)
